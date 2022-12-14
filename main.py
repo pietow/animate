ddb61db23b2c0268b23a45a43c25699f1642208c
piet <pprotassow@posteo.de>
2022-12-15 00:59:58 +0100

    takes list of lists

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import random
 
-def animate_bar(func, values):
+def animate_bar(results):
     fig = plt.figure(figsize=(8,6))
     axes = fig.add_subplot(1,1,1)
     axes.set_ylim(0, 10)
 
-    results = func(values)
+    
 
     def update(frame):
         plt.clf()
         indices = [str(i) for i in results[frame]]
         plt.bar(indices, results[frame], color='blue')
 
-    ani = FuncAnimation(fig, update, frames=range(len(results)), interval=100, 
+    ani = FuncAnimation(fig, update, frames=range(len(results)), interval=800, 
             repeat=False)
     plt.show()
 
