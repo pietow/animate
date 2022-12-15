063f79fd416dcf9c9c2523aef6c9f67fbb5ac5e5
piet <pprotassow@posteo.de>
2022-12-15 01:09:09 +0100

    pass interval

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import random
 
-def animate_bar(results):
+def animate_bar(results, interval=100):
     fig = plt.figure(figsize=(8,6))
     axes = fig.add_subplot(1,1,1)
     axes.set_ylim(0, 10)
@@ -15,7 +15,7 @@ def animate_bar(results):
         indices = [str(i) for i in results[frame]]
         plt.bar(indices, results[frame], color='blue')
 
-    ani = FuncAnimation(fig, update, frames=range(len(results)), interval=800, 
+    ani = FuncAnimation(fig, update, frames=range(len(results)), interval=interval, 
             repeat=False)
     plt.show()
 
